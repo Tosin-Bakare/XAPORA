d6ed77d9c114f8d6a35bebc5dcbe251411c5036c
Oluwatosin Bakare <oluwatosin.bakare@yahoo.com>
2021-02-28 01:22:17 +0100

    styled components

--- src/index.js
+++ src/index.js
@@ -1,16 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import {Provider} from 'react-redux';
-import App from './App';
 import { BrowserRouter } from 'react-router-dom'
+import { PersistGate } from 'redux-persist/integration/react';
 
-import store from './redux/store';
+import { store, persistor } from './redux/store';
+
+
+import App from './App';
 
 ReactDOM.render(
   <Provider store={store}>
      <React.StrictMode>
         <BrowserRouter>
-          <App />
+          <PersistGate persistor={persistor }>
+            <App />
+          </PersistGate>
          </BrowserRouter>
      </React.StrictMode>
   </Provider>,
